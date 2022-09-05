bb673488913ce1071e5c3b21eccefb2e989fd198
Stefanos Malliaros <stef.malliaros@gmail.com>
2022-09-05 15:27:46 +0100

    change token create for 1.25

--- scripts/master.sh
+++ scripts/master.sh
@@ -75,7 +75,7 @@ subjects:
   namespace: kubernetes-dashboard
 EOF
 
-# kubectl -n kubernetes-dashboard get secret "$(kubectl -n kubernetes-dashboard get sa/admin-user -o jsonpath="{.secrets[0].name}")" -o go-template="{{.data.token | base64decode}}" >> /vagrant/configs/token
+kubectl create token admin-user -n kubernetes-dashboard >> /vagrant/configs/token
 
 sudo -i -u vagrant bash << EOF
 whoami

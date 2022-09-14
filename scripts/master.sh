85dc46c41e5656d72adbd1b6fb324ff8bd0852b6
Stefanos Malliaros <stef.malliaros@gmail.com>
2022-09-14 13:10:54 +0100

    Fix Ip addressing

--- scripts/master.sh
+++ scripts/master.sh
@@ -4,9 +4,9 @@
 
 set -euxo pipefail
 
-MASTER_IP="10.0.0.10"
+MASTER_IP="192.168.56.10"
 NODENAME=$(hostname -s)
-POD_CIDR="192.168.0.0/16"
+POD_CIDR="172.18.0.0/16"
 
 sudo kubeadm config images pull
 
